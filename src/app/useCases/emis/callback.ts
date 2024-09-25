08d260c3def7d965c3a933499dd81c046d37bcd3
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2024-09-25 11:33:48 +0100

    sum value of the finance

--- src/app/useCases/emis/callback.ts
+++ src/app/useCases/emis/callback.ts
@@ -18,22 +18,35 @@ export class CallBackUseCase {
         include: { modality: true, user: true },
       });
 
-      if (status === 'success' && booking) {
+      if (status === "success" && booking) {
         await Prisma.booking.update({
           where: { reference: identifier },
           data: { status: BOOKING_STATUS.CONFIRMED },
-        })
+        });
+
+        const finance = await Prisma.finance.findFirst();
+
+        const numberValue = Math.trunc(data.amount);
+        let addValue: number;
+        if (finance) {
+          addValue = finance.value + numberValue;
+
+          await Prisma.finance.update({
+            where: { id: finance?.id },
+            data: { value: addValue },
+          });
+        }
 
         await this.notifyUserByEmail({
           amount: data.amount,
           email: booking.user.email,
           identifier,
-          description: booking.description ?? '',
+          description: booking.description ?? "",
           modality: booking.modality.name,
           name: booking.user.name,
-          phone: booking.user.phone ?? '',
-          startDate: dayjs(booking.init_date).format('DD/MM/YYYY'),
-          startTime: dayjs(booking.start_time).format('HH:mm'),
+          phone: booking.user.phone ?? "",
+          startDate: dayjs(booking.init_date).format("DD/MM/YYYY"),
+          startTime: booking.start_time,
         });
       }
 
@@ -65,7 +78,6 @@ export class CallBackUseCase {
     modality: string;
     description: string;
   }) {
-
     function formatCurrency(value: number, currencySymbol: string): string {
       const formattedValue = Number(value.toFixed(2));
       const valueWithComma = String(formattedValue).replace(".", ",");
@@ -73,7 +85,7 @@ export class CallBackUseCase {
     }
 
     return new SendMail().execute({
-      to: email ?? '',
+      to: email ?? "",
       html: `
         <!DOCTYPE html>
 <html lang="pt-BR">
@@ -100,7 +112,10 @@ export class CallBackUseCase {
             <p style="margin: 5px 0; color: #666666;"><strong>Hora da Reserva:</strong> ${startTime}</p>
             <p style="margin: 5px 0; color: #666666;"><strong>Serviço Reservado:</strong> ${modality}</p>
             <p style="margin: 5px 0; color: #666666;"><strong>Observações:</strong> ${description}</p>
-            <p style="margin: 5px 0; color: #666666;"><strong>TOTAL PAGO:</strong> ${formatCurrency(amount, "KZ")}</p>
+            <p style="margin: 5px 0; color: #666666;"><strong>TOTAL PAGO:</strong> ${formatCurrency(
+              amount,
+              "KZ"
+            )}</p>
         </div>
         <div style="text-align: center; color: #999999; font-size: 12px;">
             <p style="margin: 5px 0;">&copy; ${new Date().getFullYear()} PN Clique. Todos os direitos reservados.</p>
@@ -111,7 +126,7 @@ export class CallBackUseCase {
 </html>
 
       `,
-      subject: 'Reserva Confirmada com Sucesso!',
+      subject: "Reserva Confirmada com Sucesso!",
     });
   }
 }
