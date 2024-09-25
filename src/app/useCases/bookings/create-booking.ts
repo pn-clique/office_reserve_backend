5fa984c9a4565dff7a3b89f1c92476435d224b88
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2024-09-25 19:54:47 +0100

    add real  value

--- src/app/useCases/bookings/create-booking.ts
+++ src/app/useCases/bookings/create-booking.ts
@@ -87,7 +87,7 @@ export class CreateBookingUseCase implements UseCase {
 
       const emisIntegrationService = new EmisIntegrationService();
       const payment = await emisIntegrationService.generatePaymentReference({
-        value: 50,//amount,
+        value: amount,
         plan: `${place.name} - ${modalities.name}`,
         firstName: user.name.split(" ")[0] ?? name,
         lastName: user.name.split(" ")[1] ?? name,
